9023eea5b715b0c73d3c53391fd34ebc317aa591
HigorBraga <higgorbraga123@gmail.com>
2023-06-06 01:32:18 -0300

    Atualizando pasta inicio
    Nesta atualização contém estudos sobre operadores de precedência em javascript.

---
+++ roadmap.javascript/inicio/precedenciaoperador.js
@@ -0,0 +1,7 @@
+/*
+
+A precedência do operador descreve a ordem na qual as operações são executadas em uma expressão aritmética.
+
+A multiplicação ( *) e a divisão ( /) têm maior precedência do que a adição ( +) e a subtração ( -).
+
+*/
