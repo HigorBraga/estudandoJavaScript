b8d5bf760f1f8f8e039fa836af8e284f7ecb3cc5
Higor Gustavo Martins Braga <98365126+HigorBraga@users.noreply.github.com>
2023-06-09 02:53:32 -0300

    Leitura sobre as versões do Js
    Nesta atualização contém algumas leituras sobre as versões do JavaScript.

---
+++ roadmap.javascript/VersoesJs/versoes.js
@@ -0,0 +1,11 @@
+/*
+
+O JavaScript foi inventado por Brendan Eich em 1995 e tornou-se um padrão ECMA em 1997.
+
+ECMAScript é o nome oficial da linguagem.
+
+As versões do ECMAScript foram abreviadas para ES1, ES2, ES3, ES5 e ES6.
+
+Desde 2016, as versões são nomeadas por ano (ECMAScript 2016, 2017, 2018, 2019, 2020).
+
+*/
