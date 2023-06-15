8bf7eda2373371905a06c65e6b368da48b8160fc
HigorBraga <higgorbraga123@gmail.com>
2023-06-15 02:04:31 -0300

    Conjuntos sets objetos
    Nesta atualização contém estudos sobre conjuntoo de objetos em objetos.

---
+++ roadmap.javascript/Objetos-Js/conjuntossetsobjetos.js
@@ -0,0 +1,63 @@
+/*
+
+Um Conjunto JavaScript é uma coleção de valores únicos.
+
+Cada valor só pode ocorrer uma vez em um Conjunto.
+
+Um conjunto pode conter qualquer valor de qualquer tipo de dados.
+
+
+
+
+Método	            Descrição
+novo conjunto()	    Cria um novo Conjunto
+adicionar()	        Adiciona um novo elemento ao Conjunto
+excluir()	        Remove um elemento de um Conjunto
+claro()	            Remove todos os elementos de um conjunto
+para cada()	        Chama um retorno de chamada para cada elemento
+tem()	            Retorna verdadeiro se existir um valor
+valores()	        Retorna um Iterator com todos os valores em um Set
+chaves()	        O mesmo que valores()
+entradas()	        Retorna um Iterator com os pares [value,value] de um Set
+
+
+Propriedade	        Descrição
+tamanho	            Retorna os elementos numéricos em um Conjunto
+
+
+
+Como criar um conjunto
+Você pode criar um Conjunto de JavaScript por:
+
+Passando um Array paranew Set()
+Crie um novo conjunto e use add()para adicionar valores
+Crie um novo conjunto e use add()para adicionar variáveis.
+
+
+
+O método forEach()
+O forEach()método invoca uma função para cada elemento Set.
+
+
+
+O método values()
+O values()método retorna um objeto Iterator contendo todos os valores em um Set.
+
+
+
+O método keys()
+Um Conjunto não tem chaves.
+
+keys()retorna o mesmo que values().
+
+Isso torna os Sets compatíveis com o Maps.
+
+
+
+O método entry()
+Um Conjunto não tem chaves.
+
+entries()retorna pares [valor, valor] em vez de pares [chave, valor].
+
+Isso torna os Conjuntos compatíveis com o Maps.
+*/
